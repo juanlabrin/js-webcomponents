2a175adf2499ced092ad54b870a0354490bb61f7
Juan Paulo Labrin <juanlabrin@gmail.com>
2024-03-23 17:56:35 -0300

    TTB | Update 2023 Version

--- public/js/timeline-task-box/timeline-task-box.2023.class.js
+++ public/js/timeline-task-box/timeline-task-box.2023.class.js
@@ -1,156 +1,337 @@
 const template = document.createElement('template');
-const divBox = document.createElement('div');
-const tableTimeline = document.createElement('table');
-divBox.id = 'ttb-container';
-tableTimeline.id = 'ttb-table';
-divBox.appendChild(tableTimeline);
-template.appendChild(divBox);
-
+template.innerHTML = `
+<style>
+@import "/css/timeline-task-box.css";
+</style>
+<div class="wrap">
+    <div class="timeline-header">
+        <h5 class="timeline-month-name">{Month Name}</h5>
+        <select class="weeks-with-tasks">{Week Range}</select>
+    </div>
+    <div class="timeline-container">
+        <div class="arrow prev-day">prev</div>
+        <div class="timeline-grid">
+            <div class="week-grid"></div>
+            <div class="tasks-grid"></div>
+        </div>
+        <div class="arrow next-day">next</div>
+    </div>
+</div>
+`;
 class TimelineTaskBox extends HTMLElement {
     constructor() {
         super();
-        // Set copyright info
 
-        // Attach shadow root to class
         this._shadowRoot = this.attachShadow({ mode: 'open' });
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-        // Create link stylesheet and add to class
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = '/css/ttb-styles.css';
-        this._shadowRoot.appendChild(link);
+        this.$prevDay = this._shadowRoot.querySelector(".prev-day");
+        this.$nextDay = this._shadowRoot.querySelector(".next-day");
 
-        // Get template content
-        this._shadowRoot.appendChild(template.firstChild);
+        this.$monthName = this._shadowRoot.querySelector(".timeline-month-name");
+        this.$weeksWithTasks = this._shadowRoot.querySelector(".weeks-with-tasks");
+        this.$weekGrid = this._shadowRoot.querySelector(".week-grid");
+        this.$tasksGrid = this._shadowRoot.querySelector(".tasks-grid");
 
-        // Instantiate objects
-        this.$ttbContainer = this._shadowRoot.getElementById('ttb-container');
-        this.$ttbTable = this._shadowRoot.getElementById('ttb-table');
-
+        this.$months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        this.$days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        this.$drawOptionWeeks = true;
         this.$date = new Date();
-        this.$task = '';
-        this.$weekDatesArray = [];
+        this.$data = [];
+    }
+
+    async _sendData(url, data) {
+        let options = {
+            method: 'post',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        // console.log(options);
+        const response = await fetch(url, options);
+        const json = await response.json();
+        return json;
+    }
+
+    async _getData(url) {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
     }
 
     _getWeekDates(date) {
         let newDate = new Date(date);
         let weekDatesArray = [];
-
         let startOfWeek = newDate.setDate(newDate.getDate() - newDate.getDay());
         let endOfWeek = newDate.setDate(newDate.getDate() + (6 - newDate.getDay()));
-
         for (let d = new Date(startOfWeek); d <= endOfWeek; d.setDate(d.getDate() + 1)) {
             weekDatesArray.push(new Date(d));
         }
-
         return weekDatesArray;
     }
 
-    _drawTable(weekDatesArray, taskDate, taskTitle) {
-        console.log(typeof taskDate, taskDate.toUTCString());
-        let tBody = document.createElement('tbody');
-        let row = tBody.insertRow();
-        let draggableDiv = document.createElement('div');
+    _drawModal(task){
+        // console.log(task);
+        let options = ['NOT SET', 'CREATED', 'IN PROCESS', 'ON HOLD', 'COMPLETED', 'NEED INTERVENTION', 'CANCELLED'];
+        
+        let modal = document.createElement('div');
+        let modalContent = document.createElement('div');
+        let taskEditBox = document.createElement('div');
+        
+        let modalTitle = document.createElement('h5');
+        let btnEdit = document.createElement('button');
 
-        let isDragging = false;
-        let initialMouseX, initialMouseY, initialElementX, initialElementY;
+        let inputDivId = document.createElement('input');
+        let inputDivTitle = document.createElement('input');
+        let inputDivStatus = document.createElement('select');
+        let inputDivInitDate = document.createElement('input');
+        let inputDivLimitDate = document.createElement('input');
 
-        draggableDiv.classList.add('draggable');
-        draggableDiv.textContent = `${taskDate.getUTCDate()}/${taskDate.getUTCMonth() + 1} - ${taskTitle}`;
-
-        for (const date of weekDatesArray) {
-            let cell = row.insertCell();
-            cell.textContent = date.getUTCDate();
-            if (date.getUTCDate() === taskDate.getUTCDate()) {
-                cell.id = `actual-date`;
-            }
+        for (const o of options) {
+            let option = document.createElement('option');
+            option.value = o;
+            option.textContent = o;
+            inputDivStatus.appendChild(option);
         }
 
-        this.$ttbTable.appendChild(tBody);
-        this.$ttbContainer.appendChild(draggableDiv);
+        inputDivId.value = task._id;
+        inputDivTitle.value = task.title;
+        inputDivStatus.value = task.status;
+        inputDivInitDate.value = task.initDate.split('T')[0];
+        inputDivLimitDate.value = (task.limitDate != null)?task.limitDate.split('T')[0]:task.initDate.split('T')[0];
 
-        let parentRec = this.$ttbContainer.getBoundingClientRect();
-        let draggableRec = draggableDiv.getBoundingClientRect();
-        let childRec = this.$ttbTable.querySelector('#actual-date').getBoundingClientRect();
+        inputDivId.type = 'hidden';
+        inputDivInitDate.type = 'date';
+        inputDivLimitDate.type = 'date';
 
-        console.log('containerRec', parentRec);
-        console.log('draggableRec', draggableRec);
-        console.log('cellRec', childRec);
+        inputDivTitle.style.gridColumn = '1/5';
 
-        draggableDiv.style.top = (childRec.top - parentRec.top) + 2;
-        draggableDiv.style.left = Math.ceil(draggableRec.x + childRec.x);
+        modalTitle.textContent = 'Edit Task';
+        btnEdit.textContent = 'Save';
 
-        draggableDiv.addEventListener('mousedown', function (e) {
-            isDragging = true;
-            initialMouseX = e.clientX;
-            initialMouseY = e.clientY;
-            const elementRect = draggableDiv.getBoundingClientRect();
-            initialElementX = elementRect.left - parentRec.left;
-            initialElementY = elementRect.top - parentRec.top;
-            console.log(initialMouseX, initialMouseY, initialElementX, initialElementY);
+        btnEdit.addEventListener('click', async (e) => {
+            console.log(inputDivId.value);
+            let set = {
+                title: inputDivTitle.value,
+                status: inputDivStatus.value,
+                initDate: inputDivInitDate.value,
+                limitDate: inputDivLimitDate.value
+            }
+            let response = await this._sendData('/tasks/update', { _id: inputDivId.value, set: set });
+            if(response.success){
+                this.refresh();
+                modal.remove();
+            }
         });
 
-        document.addEventListener('mousemove', function (e) {
-            if (!isDragging) return;
-            const deltaX = e.clientX - initialMouseX;
-            const deltaY = e.clientY - initialMouseY;
+        taskEditBox.appendChild(modalTitle);
+        taskEditBox.appendChild(inputDivId);
+        taskEditBox.appendChild(inputDivTitle);
+        taskEditBox.appendChild(inputDivStatus);
+        taskEditBox.appendChild(inputDivInitDate);
+        taskEditBox.appendChild(inputDivLimitDate);
 
-            const newElementX = initialElementX + deltaX;
-            const newElementY = initialElementY + deltaY;
+        btnEdit.classList.add('timeline-modal-btn-edit');
+        taskEditBox.classList.add('timeline-modal-task-edit-box');
+        modalContent.classList.add('timeline-modal-content');
+        modal.classList.add('timeline-modal');
 
-            // const containerRect = parentRec.getBoundingClientRect();
+        taskEditBox.appendChild(btnEdit);
+        modalContent.appendChild(taskEditBox);
+        modal.appendChild(modalContent);        
+        document.getElementsByTagName('body')[0].appendChild(modal);
 
-            draggableDiv.style.left = newElementX + 'px';
-            draggableDiv.style.top = newElementY + 'px';
+        window.addEventListener('click', (e) => {
+            if(e.target === modal){
+                modal.remove();
+            }
+        });
+    }
+
+    _drawTimeline(tasks, week) {
+        // console.log(tasks, week);
+
+        this.$weekGrid.innerHTML = '';
+        this.$tasksGrid.innerHTML = '';
+
+        week.forEach(d => {
+            let day = document.createElement('div');
+            day.classList.add('day');
+            day.textContent = `${this.$days[d.getUTCDay()]}, ${d.getUTCDate()}`;
+            this.$weekGrid.appendChild(day);
         });
 
-        document.addEventListener('mouseup', () => {
-            isDragging = false;
+        if (tasks.length > 0) {
+            tasks.forEach(t => {
+                let task = document.createElement('div');
+                let taskTitle = document.createElement('span');
+                let taskStatus = document.createElement('span');
+
+                task.classList.add('task');
+                task.style.gridColumnStart = t.lineStart;
+                task.style.gridColumnEnd = t.lineEnd;
+                task.style.backgroundColor = t.task.taskColor;
+
+
+                taskStatus.classList.add('status-badge');
+                (t.task.status === 'CREATED') ? taskStatus.classList.add('status-badge-created') : '';
+                (t.task.status === 'COMPLETED') ? taskStatus.classList.add('status-badge-completed') : '';
+                (t.task.status === 'IN PROCESS') ? taskStatus.classList.add('status-badge-inprocess') : '';
+                (t.task.status === 'NOT SET') ? taskStatus.classList.add('status-badge-notset') : '';
+
+                taskTitle.textContent = t.task.title;
+                taskStatus.textContent = t.task.status;
+
+                task.addEventListener('click', (e) => { this._drawModal(t.task); });
+
+                task.appendChild(taskTitle);
+                task.appendChild(taskStatus);
+                this.$tasksGrid.append(task);
+            });
+        } else {
+            let task = document.createElement('div');
+            let taskTitle = document.createElement('span');
+            let taskStatus = document.createElement('span');
+
+            task.classList.add('task');
+            task.style.gridColumnStart = 1;
+            task.style.gridColumnEnd = 8;
+
+            taskTitle.textContent = "Test";
+            taskStatus.textContent = "Test";
+
+            task.appendChild(taskTitle);
+            task.appendChild(taskStatus);
+            this.$tasksGrid.append(task);
+        }
+
+
+    }
+
+    _setPrevNextArrows(currentWeek) {
+        this.$prevDay.innerHTML = '';
+        this.$nextDay.innerHTML = '';
+
+        let prevDay = currentWeek[0].getTime() - (24 * 60 * 60 * 1000);
+        let nextDay = currentWeek[6].getTime() + (24 * 60 * 60 * 1000);
+
+        // console.log(new Date(prevDay), new Date(nextDay));
+
+        let btnPrevDay = document.createElement('button');
+        btnPrevDay.setAttribute('rel', prevDay);
+        btnPrevDay.innerHTML = '&#x276E;';
+        btnPrevDay.addEventListener('click', (e) => { this._proccesData(new Date(prevDay), this.$data); });
+        this.$prevDay.appendChild(btnPrevDay);
+
+        let btnNextDay = document.createElement('button');
+        btnNextDay.setAttribute('rel', nextDay);
+        btnNextDay.innerHTML = '&#x276F;';
+        btnNextDay.addEventListener('click', (e) => { this._proccesData(new Date(nextDay), this.$data); });
+        this.$nextDay.appendChild(btnNextDay);
+    }
+
+    _formatDate(string) {
+        let date = new Date(string);
+        function addZero(int) {
+            if (int < 10) {
+                return '0' + int;
+            } else {
+                return int;
+            }
+        }
+        date = `${date.getUTCFullYear()}${addZero(parseInt(date.getUTCMonth() + 1))}${addZero(parseInt(date.getUTCDate()))}`
+        return date;
+    }
+
+    _getWeeksWithTasks(dates){
+        this.$weeksWithTasks.innerHTML = '';
+        let weeksWithTasks = [];
+
+        for(const date of dates){
+            let week = this._getWeekDates(date);
+            weeksWithTasks.push({ text: `Week[${week[0].getUTCDate()}/${(week[0].getUTCMonth() + 1)} - ${week[6].getUTCDate()}/${(week[6].getUTCMonth() + 1)}]`, date: date.split('T')[0] });
+        }
+
+        let filtered = weeksWithTasks.filter(function({text}){
+            var key = `${text}`;
+            return !this.has(key) && this.add(key);
+        }, new Set);
+
+        for(const week of filtered){
+            let option = document.createElement('option');
+            option.value = week.date;
+            option.textContent = week.text;
+            this.$weeksWithTasks.appendChild(option);
+        }
+
+        this.$weeksWithTasks.addEventListener('change', (e) => {
+            e.preventDefault();
+            this._proccesData(new Date(this.$weeksWithTasks.value), this.$data);
         });
 
-        document.addEventListener('mouseleave', () => {
-            isDragging = false;
-        });
-
-        console.log('draggableRec new position', draggableDiv.getBoundingClientRect());
-        // console.log(this.$ttbTable.getBoundingClientRect());
-        // console.log(this.$ttbContainer.getBoundingClientRect());
+        this.$drawOptionWeeks = false;
     }
 
-    get taskDate() {
-        return new Date(this.getAttribute('task-date'));
+    _proccesData(date, data) {
+        let currentWeek = this._getWeekDates(date);
+        let tasksInCurrentWeek = [];
+
+        let daysWithTasks = new Set();
+
+        for (const task of data) {
+            let indexStart = currentWeek.findIndex((date) => this._formatDate(date.toUTCString()) === this._formatDate(task.initDate));
+            if (indexStart != -1) {
+                indexStart = indexStart + 1;
+                let indexEnd = indexStart + 1;
+                if (task.limitDate != null) {
+                    indexEnd = currentWeek.findIndex((date) => this._formatDate(date.toUTCString()) === this._formatDate(task.limitDate));
+                    if (indexEnd != -1) {
+                        indexEnd = indexEnd + 2;
+                    }
+                }
+                tasksInCurrentWeek.push({ lineStart: indexStart, lineEnd: indexEnd, task: task });
+            }
+            daysWithTasks.add(task.initDate);
+        }
+
+        if(this.$drawOptionWeeks){
+            this._getWeeksWithTasks(daysWithTasks);
+        }        
+
+        this._drawTimeline(tasksInCurrentWeek, currentWeek);
+        this._setPrevNextArrows(currentWeek);
+        this.$monthName.textContent = this.$months[new Date(date).getUTCMonth()];
     }
 
-    set taskDate(url) {
-        this.setAttribute('task-date', url);
+    async _loadData(dataSource) {
+        let response = await this._getData(dataSource);
+        if (response.success) {
+            ;
+            this.$data = response.data;
+            this._proccesData(this.$date, this.$data);
+        }
     }
 
-    get taskTitle() {
-        return this.getAttribute('task-title');
+    get dataSource() {
+        return this.getAttribute('data-source');
     }
 
-    set taskTitle(url) {
-        this.setAttribute('task-title', url);
+    set dataSource(dataSource) {
+        this.setAttribute('data-source', dataSource);
+    }
+
+    refresh() {
+        this._loadData(this.dataSource);
     }
 
     connectedCallback() {
         console.log('Timeline Task Box Connected!');
-
-        if (this.hasAttribute('task-date')) {
-            this.$date = this.taskDate;
-            this.$weekDatesArray = this._getWeekDates(this.$date);
-        } else {
-            this.$weekDatesArray = this._getWeekDates(this.$date);
-        }
-
-        if (this.hasAttribute('task-title')) {
-            this.$taskTitle = this.taskTitle;
-        }
-
-        if (this.$weekDatesArray.length > 0) {
-            this._drawTable(this.$weekDatesArray, this.$date, this.$taskTitle);
+        if (this.hasAttribute('data-source')) {
+            this._loadData(this.dataSource);
         }
     }
+
 }
 
 customElements.define('timeline-task-box', TimelineTaskBox);
